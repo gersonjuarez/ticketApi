29a0b552f82de58d2a51f25cc6308f0b1045a449
Luisdari5455 <luisdari5455@gmail.com>
2025-08-17 21:49:31 -0600

    change models

--- models/service.js
+++ models/service.js
@@ -1,45 +1,49 @@
+// models/service.js
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
-    class Service extends Model {
-        static associate(models) {
-            Service.hasMany(models.Cashier, {
-                foreignKey: "idService",
-            });
-            Service.hasMany(models.TicketRegistration, {
-                foreignKey: "idService",
-            });
-        }
+  class Service extends Model {
+    static associate(models) {
+      Service.hasMany(models.Cashier, { foreignKey: "idService" });
+      Service.hasMany(models.TicketRegistration, { foreignKey: "idService" });
     }
-    Service.init(
-        {
-            idService: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: DataTypes.INTEGER,
-            },
-            name: {
-                type: DataTypes.STRING(100),
-                allowNull: false,
-            },
-            prefix: {
-                type: DataTypes.STRING(15),
-            },
-            value: {
-                type: DataTypes.STRING(5),
-                allowNull: false,
-            },
-            status: {
-                type: DataTypes.BOOLEAN,
-                allowNull: false,
-                defaultValue: true,
-            },
-        },
-        {
-            sequelize,
-            modelName: "Service",
-            timestamps: true,
-        }
-    );
-    return Service;
+  }
+
+  Service.init(
+    {
+      idService: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      prefix: {
+        type: DataTypes.STRING(15),
+      },
+      value: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      // createdAt / updatedAt los maneja Sequelize con timestamps:true
+    },
+    {
+      sequelize,
+      modelName: "Service",
+      tableName: "services",   // <-- EXACTO como en MySQL
+      timestamps: true,
+      underscored: false,
+      freezeTableName: true,   // evita pluralización automática
+    }
+  );
+
+  return Service;
 };
