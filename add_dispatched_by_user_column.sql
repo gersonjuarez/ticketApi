0427c12502c27c726fcc5a972d1b997bfd505909
gersonjuarez <gjuarezjuarez.97@hotmail.com>
2025-08-19 20:34:20 -0600

    feature()

--- add_dispatched_by_user_column.sql
+++
@@ -1,8 +0,0 @@
--- Agregar columna dispatchedByUser a la tabla ticketregistrations
-ALTER TABLE ticketregistrations 
-ADD COLUMN dispatchedByUser INT NULL 
-COMMENT 'ID del usuario que despachó el ticket';
-
--- Agregar clave foránea (opcional, depende de tu configuración)
--- ALTER TABLE ticketregistrations 
--- ADD FOREIGN KEY (dispatchedByUser) REFERENCES users(idUser);
