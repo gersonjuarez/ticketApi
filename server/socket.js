8b6fd32aca13bdf70e4472ac7f5f46cb6207740d
Luis David <luisdari5455@gmail.com>
2025-11-21 15:57:25 -0600

    cambio de socket

--- server/socket.js
+++ server/socket.js
@@ -540,11 +540,6 @@ function init(httpServer, opts = {}) {
 // BRIDGE – registro correcto con backend
 // ============================================
 
-const io = require("socket.io-client");
-const SOCKET_URL = "http://localhost:3001"; 
-// O tu URL real:
-// const SOCKET_URL = "https://tuservidor.com";
-
 // LEER locationId correctamente
 const locationId =
   process.env.LOCATION_ID ||
